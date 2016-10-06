5b8a2521fcbad1414a4d0172fb43d9f37dcb7f47
Thomas Swann <tkswann@mac.com>
2016-10-06 00:10:13 -0500

    intial redux up and working

--- src/app/index.js
+++ src/app/index.js
@@ -35,11 +35,28 @@ import { createStore } from 'redux'
 const reducer = (state,action) => {
   switch(action.type) {
     case "ADD":
+      state = state + action.payload
       break
-    case: "SUBTRACT":
+    case "SUBTRACT":
+    state = state - action.payload
       break
-    default
   }
   return state
 }
 const store = createStore(reducer, 1)
+store.subscribe(() => {
+  console.log('store updated!', store.getState())
+})
+
+store.dispatch({
+  type: "ADD",
+  payload: 100
+})
+store.dispatch({
+  type: "ADD",
+  payload: 22
+})
+store.dispatch({
+  type: "SUBTRACT",
+  payload: 80
+})
